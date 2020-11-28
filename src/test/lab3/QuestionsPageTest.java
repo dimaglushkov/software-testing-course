1e99a3753f57bf905a2bfb95276b75237a6f1b24
allacee <ddimaglushkov@ya.ru>
2020-11-28 22:12:01 +0300

    Add more tests

--- src/test/lab3/QuestionsPageTest.java
+++ src/test/lab3/QuestionsPageTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsProvider;
 import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.openqa.selenium.WebElement;
 import test.lab3.pages.ChromeDriverInitializer;
 import test.lab3.pages.FirefoxDriverInitializer;
 import test.lab3.pages.QuestionsPage;
@@ -24,7 +25,7 @@ public class QuestionsPageTest {
     @BeforeAll
     static void setup() {
         firefoxHomePage = new QuestionsPage(FirefoxDriverInitializer.getWebDriver());
-        chromeHomePage = new QuestionsPage(ChromeDriverInitializer.getWebDriver());
+//        chromeHomePage = new QuestionsPage(ChromeDriverInitializer.getWebDriver());
     }
 
     @ParameterizedTest
@@ -49,10 +50,72 @@ public class QuestionsPageTest {
         assertEquals("https://www.answers.com/u/trshmil", page.getQuestionAuthor(latestQuestion));
     }
 
+    @ParameterizedTest
+    @ArgumentsSource(WebdriverArgumentsProvider.class)
+    void onlineEditorTextFormattingTest(QuestionsPage page) {
+        WebElement editor = page.getOnlineEditor(latestQuestion);
+        WebElement boldButton = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[3]/span[1]");
+        WebElement underlinedButton = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[3]/span[2]");
+        WebElement italicButton = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[3]/span[3]");
+        editor.sendKeys("first row(normal)\n");
+
+        boldButton.click();
+        editor.sendKeys("second row(bold)\n");
+        boldButton.click();
+
+        underlinedButton.click();
+        editor.sendKeys("third row(underlined)\n");
+        underlinedButton.click();
+
+        italicButton.click();
+        editor.sendKeys("fourth row(italic)\n");
+        italicButton.click();
+
+        boldButton.click();
+        underlinedButton.click();
+        italicButton.click();
+        editor.sendKeys("fifth row(all)");
+        italicButton.click();
+
+
+        WebElement boldRow = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[1]/div/div/div/div[2]/div/span");
+        WebElement underlinedRow = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[1]/div/div/div/div[3]/div/span");
+        WebElement italicRow = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[1]/div/div/div/div[4]/div/span");
+        WebElement allRow = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[1]/div/div/div/div[5]/div/span");
+
+        assertTrue(boldRow.getAttribute("style").contains("bold"));
+        assertTrue(underlinedRow.getAttribute("style").contains("underline"));
+        assertTrue(italicRow.getAttribute("style").contains("italic"));
+        assertTrue(allRow.getAttribute("style").contains("bold")
+                && allRow.getAttribute("style").contains("underline")
+                && allRow.getAttribute("style").contains("italic"));
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(WebdriverArgumentsProvider.class)
+    void onlineEditorListsTest(QuestionsPage page) {
+        WebElement editor = page.getOnlineEditor(latestQuestion);
+        WebElement numberedListButton = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[3]/span[4]");
+        WebElement checkListButton = page.find("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[3]/span[5]");
+
+        numberedListButton.click();
+        editor.sendKeys("first numbered row\nsecond numbered row");
+        assertEquals(2, page.findElements("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[1]/div/div/div/ol/li").size());
+
+        editor.sendKeys("\n");
+        numberedListButton.click();
+
+        checkListButton.click();
+        editor.sendKeys("first checked row\nsecond checked row");
+        assertEquals(2, page.findElements("/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[1]/div/div/div/ul/li").size());
+        editor.sendKeys("\n");
+        checkListButton.click();
+    }
+
     @AfterAll
     static void closeup() {
         FirefoxDriverInitializer.getWebDriver().close();
-        ChromeDriverInitializer.getWebDriver().close();
+//        ChromeDriverInitializer.getWebDriver().close();
     }
 
     static class WebdriverArgumentsProvider implements ArgumentsProvider {
--- src/test/lab3/pages/QuestionsPage.java
+++ src/test/lab3/pages/QuestionsPage.java
@@ -2,8 +2,11 @@ package test.lab3.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class QuestionsPage extends HomePage {
 
@@ -11,13 +14,21 @@ public class QuestionsPage extends HomePage {
     final static String questionTextFieldXPath = "/html/body/div[3]/div[3]/div/div[2]/div[1]/div/textarea";
     final static String questionSubmitXPath = "/html/body/div[3]/div[3]/div/div[2]/button[2]";
     final static String questionAuthorXPath = "/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div/a[2]";
-    final static String answerQuestionButtonXPath = "/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[3]/div/button";
-    final static String answerTextFieldXPath = "/html/body/div[3]/div[3]/div/div[2]/div[1]/div[1]/div[2]/div";
+    final static String answerQuestionButtonXPath = "/html/body/div[1]/div/div/div[1]/div[2]/div[1]/div[2]/div[1]/div/div[1]/div[3]/div/button";
+    final static String answerTextFieldXPath = "/html/body/div[3]/div[3]/div/div/div[2]/div[1]/div[1]/div[2]/div";
     final static String answerSubmitXPath = "/html/body/div[3]/div[3]/div/div[2]/button";
 
     public QuestionsPage(WebDriver webDriver) {
         super(webDriver);
         login("trshmil", "3MUrSKK6EDbj4sN");
+    }
+
+    public WebElement find(String xpath){
+        return webDriver.findElement(By.xpath(xpath));
+    }
+
+    public List<WebElement> findElements(String xpath){
+        return webDriver.findElements(By.xpath(xpath));
     }
 
     public String createQuestion(String text){
@@ -34,14 +45,8 @@ public class QuestionsPage extends HomePage {
     }
 
     public boolean answerQuestion(String questionUrl, String text) {
-        webDriver.get(questionUrl);
-
-        WebDriverWait wait = new WebDriverWait(webDriver,10);
-
-        webDriver.findElement(By.xpath(answerQuestionButtonXPath)).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(answerTextFieldXPath)));
-        webDriver.findElement(By.xpath(answerTextFieldXPath)).click();
-        webDriver.findElement(By.xpath(answerTextFieldXPath)).sendKeys(text);
+        WebElement editor = getOnlineEditor(questionUrl);
+        editor.sendKeys(text);
         webDriver.findElement(By.xpath(answerSubmitXPath)).click();
 
         return true;
@@ -55,6 +60,18 @@ public class QuestionsPage extends HomePage {
         webDriver.findElement(By.xpath(questionAuthorXPath)).click();
 
         return webDriver.getCurrentUrl();
+    }
+
+    public WebElement getOnlineEditor(String questionUrl){
+        webDriver.get(questionUrl);
+
+        WebDriverWait wait = new WebDriverWait(webDriver,10);
+
+        webDriver.findElement(By.xpath(answerQuestionButtonXPath)).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(answerTextFieldXPath)));
+        WebElement editor = webDriver.findElement(By.xpath(answerTextFieldXPath));
+        editor.click();
+        return editor;
     }
 
 }
